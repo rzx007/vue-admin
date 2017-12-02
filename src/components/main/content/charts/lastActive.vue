1075c25a8ca9d6af37f5d9b4384bcc2dc051da0c
rzx100 <770304867@qq.com>
2017-12-02 17:32:58 +0800

    mock后台数据配置，详见webpack.dev.config.js

--- src/components/main/content/charts/lastActive.vue
+++ src/components/main/content/charts/lastActive.vue
@@ -3,19 +3,21 @@
 		<div class="col-sm-12 drag_handle aa">
 			<h3 class="text-left">最新动态</h3>
 		</div>
-		<div class="col-sm-4">
-			<div class="last_active">
-				<p class="title">
-					<img src="../../../../assets/logo.png" alt="avtrel" class="img-circle avtrel" height="30px" width="30px" />
-					<span title="username">愤怒的倒霉熊</span>
-				</p>
-				<div class="introduce">
-					<h5>标题</h5>
-					<p class="content">将回调延迟到下次 DOM 更新循环之后执行。。。。</p>
-					<p class="time text-right">2017-12-02</p>
+		<template v-for="item in lastList">
+			<div class="col-sm-4">
+				<div class="last_active">
+					<p class="title">
+						<img src="../../../../assets/logo.png" alt="avtrel" class="img-circle avtrel" height="30px" width="30px" />
+						<span title="username">{{item.username}}</span>
+					</p>
+					<div class="introduce">
+						<h5>{{item.title}}</h5>
+						<p class="content">{{item.content}}</p>
+						<p class="time text-right">{{item.time}}</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</template>
 		<div class="col-sm-4">
 			<div class="last_active">
 				<p class="title">
@@ -34,10 +36,17 @@
 <script>
 	export default {
 		name: "acvtive",
-		created(){
-			this.$http.get("/api/lastActive").then(function(res){
-				console.log(res);
-			},function(err){
+		data() {
+			return {
+				lastList: [],
+			}
+		},
+		created() {
+			this.$http.get("/api/lastActive").then(function(res) {
+				if(res.body.errno == 0) {
+					this.lastList = res.body.data;
+				}
+			}, function(err) {
 				console.log(err)
 			})
 		}
@@ -46,12 +55,14 @@
 </script>
 
 <style>
-	.listWrap{
+	.listWrap {
 		margin-bottom: 18px;
 	}
-	.listWrap .col-sm-4{
+	
+	.listWrap .col-sm-4 {
 		margin-bottom: 18px !important;
 	}
+	
 	.last_active {
 		background: white;
 		padding: 10px 10px;
@@ -59,18 +70,22 @@
 		overflow: hidden;
 		box-shadow: 5px 5px 10px silver;
 	}
+	
 	.avtrel {
 		vertical-align: middle;
 	}
-	.introduce h5{
+	
+	.introduce h5 {
 		font-weight: 800;
 	}
-	.content{
+	
+	.content {
 		margin: 10px 0;
 		height: 60px;
 		overflow: hidden;
 	}
-	.time{
+	
+	.time {
 		color: #9D9D9D;
 	}
 </style>
